62680c5e23766eb92126ea8d11bc1487313397f0
nhanhaloli <thanhnhanpro.pro@gmail.com>
2019-04-17 13:12:45 +0700

    chu thich chuc nang trong file js

--- _nhandt/_html/Calculator/calculator.js
+++ _nhandt/_html/Calculator/calculator.js
@@ -5,22 +5,28 @@ function a(value) {
 	return document.getElementById(value);
 }
 
+// nhập ký tự
 function setNum(number) {
 	x += number;
 	a('result').value = x;
 }
 
+
+// chức năng xóa thành phần vừa nhập vào
 function pre() {
 	y = a('result').value.length-1
 	a('result').value = a('result').value.substr(0,y);
 	x = a('result').value.substr(0,y);
 }
 
+
+// chức năng xóa màn hình
 function clear() {
 	x = '';
 	a('result').value = x;
 }
 
+// xuất kết quả 
 function result() {
 	a('result').value = eval(a('result').value);
 }
