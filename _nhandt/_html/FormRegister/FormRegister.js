30701f0e141bab8878b284800cf66472c42ec887
nhanhaloli <thanhnhanpro.pro@gmail.com>
2019-04-22 15:11:07 +0700

    hoan thien FormDangKy

--- _nhandt/_html/FormRegister/FormRegister.js
+++ _nhandt/_html/FormRegister/FormRegister.js
@@ -1,61 +1,65 @@
-function KiemTra(argument) {
-	var pass = document.getElementById('pass').value;
-	var repass = document.getElementById('Retype').value;
+function KiemTra() {
 	var name = document.getElementById('name').value;
-	
-	// kiem tra user name require
-	if(name == '') {
-		document.getElementById('error').style.display ='block';
-		return;
-	} else {
-		document.getElementById('error').style.display ='none';
-	}
-	
-	// kiem tra email
+	var pass = document.getElementById('pass').value;
+	var repass = document.getElementById('retype').value;
 	var email = document.getElementById('email').value;
-	if(!validateEmail(email)) {
-		alert('Sai email rau');
-		return;
-	}
-	
-	// kiem tra passs
-	if (pass != repass) {
-		alert('Hai mật khẩu không trùng nhau');
-		return;
-	}
-	
-	// kiem tra so y tu
+	var phone = document.getElementById('Phone').value;
+
 	var a = name.length;
 	var p = pass.lengh;
-	if (p< 6) { 
-		alert('mật khẩu phải cí ít nhất 6 kí tự'); 
+
+	// kiem tra user name require
+	if(name == '') {
+		alert('Bạn phải nhập username');
 		return;
 	}
-	
-	// kiem tra so y tu
+	if(/^[a-zA-Z0-9]*$/.test(name) == false){
+    	alert('bạn không được nhập các ký tự đặc biệt');
+    	return;
+	}
 	if (a<6 || a>9) {
 		alert('Username phải có từ 6 đến 9 kí tự');
 		return;
 	}
-	
-	// ham chekc ca
-	if(!(name)) {
-		alert('KhcheckSpecialCharacterong duoc nhap ky tu dac biet');
+
+	// kiem tra pass require
+	if (pass == '') {
+		alert('bạn phải nhập mật khẩu');
+		return;
+	}
+	if (p< 6) { 
+		alert('mật khẩu phải có ít nhất 6 kí tự'); 
+		return;
+	}
+	if (pass != repass) {
+		alert('Hai mật khẩu không trùng nhau');
+		return;
+	}
+
+	// kiem tra email require
+	if (email == '') {
+		alert('bạn phải nhập email');
+		return;
+	}
+	if(!validateEmail(email)) {
+		alert('email bạn nhập không tồn tại');
+		return;
+	}
+
+	// kiem tra phone require
+	if (phone == '') {
+		alert('bạn phải nhập số điện thoại');
+	}
+	return true;
+}
+
+function result() {
+	if (KiemTra()) {
+		alert('bạn đã đăng ký thành công');
 	}
 }
 
 function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-// kiem tra co ky tuc dac biet hay khong
-function checkSpecialCharacter(value) {
-	
-	var result = false;
-	// Todo:
-	// neu nhu chua ky tu dac biet result = true;
-	// nguoc lai result = false;
-	
-	return result;
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
 }
